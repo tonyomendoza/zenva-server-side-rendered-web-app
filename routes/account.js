d6003276be08c011c01b946a1ccae4a9d0098fc2
tonyomendoza <mendozatony@outlook.com>
2021-09-22 03:18:05 -0700

    Lesson 12 Account Page Layout

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// temporary objects
+const items = [
+    {name:'Item 1', description:'', price:10},
+    {name:'Item 2', description:'', price:20},
+    {name:'Item 3', description:'', price:15},
+    {name:'Item 4', description:'', price:50},
+    {name:'Item 5', description:'', price:35},
+    {name:'Item 6', description:'', price:100}
+  ]
+
 // default
 router.get('/', (req, res, next) => {
     const user = req.user;
@@ -10,8 +20,9 @@ router.get('/', (req, res, next) => {
     }
     
     const data = {
-        user: user
-    }
+        user: user,
+        items: items
+      }
     res.render('account', data);
 });
 
